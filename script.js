e36a2db8724b3e1bda29a4ea3475a7367d6ddc78
mikeoz3321 <mikeoz3321@gmail.com>
2014-01-20 22:10:32 -0800

    Next Changes.
    Got the game to work. Submit button doesn't refresh. Added outline of
    steps.

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ var trueNum = Math.floor((Math.random()*100)+1);
 
 $('#mainform').submit(function(){
 	var userGuess = $('#guessNum').val();
-	var absTest = $(Math.abs(userGuess-trueNum));
-	if absTest ==0
-		return "You Win, Nice Work";
-	else if
-		return  
+	var absTest = Math.abs(userGuess-trueNum);
 
+	if (absTest==0)
+		alert("You Win, Nice Work");
+	else if (absTest <= 5)
+		alert("You're Hot!"); 
+	else if (absTest > 5 && absTest <=15)
+		alert("You're Warm!");
+	else
+		alert("You're Cold");
+
+	return false;
 })
 
 })
