92aaa5b454ddbd55661cf8c8fa4157ee158610d8
mikeoz3321 <mikeoz3321@gmail.com>
2014-01-27 22:11:20 -0800

    Added some styling and game play
    Game kind of works now. Submit box also looks different.

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 $(document).ready(function(){
 
 var trueNum = Math.floor((Math.random()*100)+1);
+var submitCount = 0
 
 $('#mainform').submit(function(){
 	var userGuess = $('#guessNum').val();
 	var absTest = Math.abs(userGuess-trueNum);
+	submitCount ++;
+	
+	if (submitCount==1){
+		if (absTest==0)
+			alert("You Win, Nice Work");
+		else if (absTest <= 5)
+			alert("You're Hot!"); 
+		else if (absTest > 5 && absTest <=15)
+			alert("You're Warm!");
+		else
+			alert("You're Cold");
 
-	if (absTest==0)
-		alert("You Win, Nice Work");
-	else if (absTest <= 5)
-		alert("You're Hot!"); 
-	else if (absTest > 5 && absTest <=15)
-		alert("You're Warm!");
-	else
-		alert("You're Cold");
+		absTest1 = absTest
+		return false;
+		
+	}
 
-	return false;
+	else if (submitCount>1){
+		if (absTest==0)
+			alert("You Win, Nice Work");
+		else if (absTest > absTest1)
+			alert("Getting Colder, Try Again");
+		else if (absTest < absTest1)
+			alert("Getting Warmer, Keep Going!");
+		else 
+			alert ("Uh Oh");
+
+		absTest1 = absTest
+		return false;
+
+	}
+		
+
 })
 
 })
